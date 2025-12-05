40d9ea101d91b59a806149eb33aeeebcaaa21984
Artjom Saymon Semonoff <semonoff@gmail.com>
2025-12-05 18:19:41 +1100

    fix(utils): change log level from INFO to WARNING
    - Updated LOG_LEVEL constant to 'WARNING' for less verbose logging
    - Adjusted logging configuration to reduce informational messages

--- utils/constants.py
+++ utils/constants.py
@@ -2,7 +2,7 @@
 Константы приложения
 """
 
-VERSION = "0.3.0"
+VERSION = "0.3.1"
 APP_NAME = "Cone App"
 DESCRIPTION = "Приложение для построения треугольников и расчета объемов конусов"
 GITHUB_URL = "https://github.com/SemonoffArt/cone"
@@ -13,7 +13,7 @@ COPYRIGHT = "Copyright © 2025 Semonoff Art"
 EMAIL = "semonoff@gmail.com"
 
 # (DEBUG, INFO, WARNING, ERROR, CRITICAL)
-LOG_LEVEL = 'INFO' # Level of logging
+LOG_LEVEL = 'WARNING' # Level of logging
 
 # Цвета
 COLOR_TRIANGLE = "blue"
